701e91879e95fa5f17ffa8c9bcd2bbd9d6f092ff
marianadzividzinska04 <marianadzividzinska04@gmail.com>
2019-05-18 22:44:24 +0300

    Create mainwindow.h

---
+++ test/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+//#include <QPainter>
+#include <QGraphicsScene>
+
+#include "moveitem.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+
+private slots:
+    void on_pushButton_clicked();
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+};
+
+
+#endif // MAINWINDOW_H
