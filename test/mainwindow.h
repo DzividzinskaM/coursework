84692cd6140b982807133f5383d140c4710496a7
marianadzividzinska04 <marianadzividzinska04@gmail.com>
2019-05-20 11:34:12 +0300

    Update mainwindow.h

--- test/mainwindow.h
+++ test/mainwindow.h
@@ -3,10 +3,20 @@
 
 #include <QMainWindow>
 #include <QWidget>
-//#include <QPainter>
+#include <QPainter>
 #include <QGraphicsScene>
+#include <QList>
+#include <QLabel>
+#include <QPushButton>
+#include <iostream>
+#include <QLineEdit>
+#include <QtGui>
+#include <QtCore>
 
 #include "moveitem.h"
+#include "line.h"
+
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -17,16 +27,35 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    int x1_koordinate;
+    int x2_koordinate;
+    int y1_koordinate;
+    int y2_koordinate;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    int count=0;
 
 
 private slots:
     void on_pushButton_clicked();
+
+
+    void on_pushButton_2_clicked();
+
+    //void on_pushButton_3_clicked();
+
 private:
+
+
     Ui::MainWindow *ui;
     QGraphicsScene *scene;
+
+    QList<MoveItem*> vertexlist;
+//protected:
+   // void paintEvent(QPaintEvent *e);
+
 };
+
 
 
 #endif // MAINWINDOW_H
